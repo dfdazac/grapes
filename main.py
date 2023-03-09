865c052113fc5cd6b6413c83303b9d225a5abc64
Daniel <dfdazac@gmail.com>
2023-03-09 16:45:19 +0100

    Add base code for training a GCN on Cora

---
+++ main.py
@@ -0,0 +1,64 @@
+from sklearn.metrics import accuracy_score
+from tap import Tap
+import torch
+import torch.nn as nn
+from torch.optim import Adam
+from torch_geometric.datasets import Planetoid
+from tqdm import tqdm
+import wandb
+
+from modules.gcn import GCN
+
+
+class Arguments(Tap):
+    dataset: str = 'cora'
+    notes: str = None
+    log_wandb: bool = False
+
+
+def train(args: Arguments):
+    wandb.init(project='gflow-sampling',
+               mode='online' if args.log_wandb else 'disabled',
+               config=args.as_dict(),
+               notes=args.notes)
+
+    data = Planetoid(root='data/Planetoid', name=args.dataset)[0]
+    num_classes = len(data.y.unique())
+
+    model = GCN(data.num_features, hidden_dims=[32, num_classes])
+    optimizer = Adam(model.parameters(), lr=1e-2)
+    loss_fn = nn.CrossEntropyLoss()
+
+    max_epochs = 100
+    with tqdm(range(max_epochs)) as bar:
+        for epoch in bar:
+            logits = model(data.x, data.edge_index)
+            loss = loss_fn(logits[data.train_mask], data.y[data.train_mask])
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            accuracy = evaluate(logits, data.y, data.val_mask)
+            wandb.log({'valid-accuracy': accuracy})
+            wandb.log({'loss': loss.item()})
+
+            bar.set_postfix({'loss': loss.item(), 'valid_acc': accuracy},
+                            refresh=False)
+
+    test_accuracy = evaluate(logits, data.y, data.test_mask)
+    print(f'Test accuracy: {test_accuracy:.1%}')
+    wandb.log({'test-accuracy': test_accuracy})
+
+
+@torch.inference_mode()
+def evaluate(logits: torch.Tensor,
+             targets: torch.Tensor,
+             mask: torch.Tensor
+             ) -> float:
+    predictions = torch.argmax(logits, dim=1)
+    accuracy = accuracy_score(predictions[mask], targets[mask])
+    return accuracy
+
+
+train(Arguments().parse_args())
---
+++ modules/gcn.py
@@ -0,0 +1,29 @@
+import torch
+import torch.nn as nn
+from torch_geometric.nn import GCNConv
+
+
+class GCN(nn.Module):
+    def __init__(self,
+                 in_features: int,
+                 hidden_dims: list[int]):
+        super(GCN, self).__init__()
+
+        dims = [in_features] + hidden_dims
+        gcn_layers = []
+        for i in range(len(hidden_dims) - 1):
+            gcn_layers.append(GCNConv(in_channels=dims[i],
+                                      out_channels=dims[i + 1]))
+
+        gcn_layers.append(GCNConv(in_channels=dims[-2], out_channels=dims[-1]))
+        self.gcn_layers = nn.ModuleList(gcn_layers)
+
+    def forward(self,
+                x: torch.Tensor,
+                edge_index: torch.Tensor,
+                ) -> torch.Tensor:
+        for layer in self.gcn_layers[:-1]:
+            x = torch.relu(layer(x, edge_index))
+        logits = self.gcn_layers[-1](x, edge_index)
+
+        return logits
