fdf8479416b78fdcf56297d9885a59c22e5e4b4c
Taraneh Younesian <t.yoonesian@gmail.com>
2023-03-13 14:31:53 +0100

    get mini-batch indices

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from torch.optim import Adam
 from torch_geometric.datasets import Planetoid
 from tqdm import tqdm
 import wandb
+import math
 
 from modules.gcn import GCN
 
@@ -14,6 +15,7 @@ class Arguments(Tap):
     dataset: str = 'cora'
     notes: str = None
     log_wandb: bool = False
+    batch_size: int = 16
 
 
 def train(args: Arguments):
@@ -29,9 +31,21 @@ def train(args: Arguments):
     optimizer = Adam(model.parameters(), lr=1e-2)
     loss_fn = nn.CrossEntropyLoss()
 
+    train_idx = data.train_mask.nonzero()
+    train_num_batches = max(math.ceil(len(train_idx)/args.batch_size), 1)
+    train_batch_size = min(args.batch_size, len(data.train_mask))
+
+
     max_epochs = 100
     with tqdm(range(max_epochs)) as bar:
         for epoch in bar:
+            if args.batch_size > 0:
+                for batch_id in range(0, train_num_batches):
+                    if batch_id == train_num_batches-1:
+                        batch_node_idx = train_idx[batch_id * train_batch_size:]
+                    else:
+                        batch_node_idx = train_idx[batch_id*train_batch_size:(batch_id+1)*train_batch_size]
+
             logits = model(data.x, data.edge_index)
             loss = loss_fn(logits[data.train_mask], data.y[data.train_mask])
 
