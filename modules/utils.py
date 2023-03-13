d954da0c097335d1b6834a4c56dcaea263e0e268
Thiviyan Thanapalasingam <thiviyan.t@gmail.com>
2023-03-13 15:08:56 +0100

    Get all neighboring nodes

--- modules/utils.py
+++ modules/utils.py
@@ -8,7 +8,11 @@ def get_neighboring_nodes(nodes, adjecency_matrix):
     assert type(adjecency_matrix) == torch.Tensor  # adjecency_matrix should be a tensor
 
     # Get the neighboring nodes for each node in `nodes`
-    return [adjecency_matrix[node, :].nonzero().squeeze() for node in nodes]
+    x = [adjecency_matrix[node, :].nonzero().squeeze() for node in nodes]
+
+    # Convert the list of neighboring nodes to a tensor
+    x = torch.stack(x)
+    return x
 
 
 x = get_neighboring_nodes(torch.tensor([0, 1, 2]), torch.tensor([[0, 1, 1], [1, 0, 1], [1, 1, 0]]))
