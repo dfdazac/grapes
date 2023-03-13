b8b7c3b36e177dfa3965d565010d74c039842907
Emile <e.van.krieken@vu.nl>
2023-03-13 20:34:24 +0100

    feat: log-space computation of Simple for numerical stability

--- modules/simple.py
+++ modules/simple.py
@@ -1,12 +1,16 @@
-import math
-from typing import Tuple, List
 from torch.distributions import Bernoulli, constraints
 import torch
 
 
+def log1mexp(x):
+    assert (torch.all(x >= 0))
+    return torch.where(x < 0.6931471805599453094, torch.log(-torch.expm1(-x)), torch.log1p(-torch.exp(-x)))
+
+EPS = 1e-8
+
 class KSubsetDistribution(torch.distributions.ExponentialFamily):
     arg_constraints = {'probs': constraints.unit_interval}
-    def __init__(self, probs: torch.Tensor, K: int):
+    def __init__(self, probs: torch.Tensor, K: int, log_space=True):
         # See https://arxiv.org/pdf/2210.01941.pdf, in particular Algorithm 1, 2 and 5 and 6
         self.probs = probs.squeeze()
         if len(self.probs.shape) == 1:
@@ -14,44 +18,41 @@ class KSubsetDistribution(torch.distributions.ExponentialFamily):
         self._bernoulli = Bernoulli(logits=self.probs)
         self.K = K
         self.n = self.probs.shape[-1]
+        self.log_space = log_space
         self.a = self._a()
-        self.partition = self.a[..., self.n, self.K]
+        if self.log_space:
+            self.log_partition = self.a[..., self.n, self.K]
+            self.partition = self.log_partition.exp()
+        else:
+            self.partition = self.a[..., self.n, self.K]
+            self.log_partition = self.partition.log()
+
         super().__init__()
+
+
 
     def _a(self):
         # Algorithm 1 from paper
-        a = torch.zeros(self.probs.shape[:-1] + (self.n + 1, self.K + 1), device=self.probs.device)
-        a[..., 0, 0] = 1.0
+        # Code adapted from https://github.com/UCLA-StarAI/SIMPLE/blob/main/tractable_dist/k-subset_distribution.ipynb
+        #  for the log-space computation
+        a = torch.zeros(self.probs.shape[:-1] + (self.n + 1, self.K + 2), device=self.probs.device)
+        if self.log_space:
+            a = a.log()
+            log_probs = self.probs.log()
+        a[..., 0, 1] = 0.0 if self.log_space else 1.0
         for i in range(1, self.n + 1):
-            a_as_vec = a[..., i-1, :]
-            dont_take_i = a_as_vec * (1-self.probs[..., i-1].unsqueeze(-1))
-            a_j_min_1 = torch.cat([torch.zeros_like(a_as_vec[..., :1]), a_as_vec[..., :-1]], dim=-1)
-            take_i = a_j_min_1 * self.probs[..., i-1].unsqueeze(-1)
-            a[..., i, :] = dont_take_i + take_i
+            a_as_vec = a[..., i-1, 1:]
+            a_j_min_1 = a[..., i-1, :-1]
+            if self.log_space:
+                dont_take_i = a_as_vec + log1mexp(-log_probs[..., i-1].unsqueeze(-1))
+                take_i = a_j_min_1 + log_probs[..., i-1].unsqueeze(-1)
+                a[..., i, 1:] = torch.logaddexp(dont_take_i, take_i)
+            else:
+                dont_take_i = a_as_vec * (1-self.probs[..., i-1].unsqueeze(-1))
+                take_i = a_j_min_1 * self.probs[..., i-1].unsqueeze(-1)
+                a[..., i, 1:] = dont_take_i + take_i
 
-        return a
-
-    def pr_exactly_k(self, l: int, u: int, k: int) -> Tuple[torch.Tensor, List[torch.Tensor]]:
-        # Algorithm 5 from paper
-        # Note: this algorithm doesn't work, it goes into infinite recursion. I asked the authors about this.
-        if l > u:
-            return 0.0
-        if l == u:
-            assert k == 1 or k == 0
-            if k == 1:
-                return self.probs[..., l]
-            return 1 - self.probs[..., l]
-        if (l, u) in self._cache:
-            return self._cache[(l, u)]
-        pm = []
-        fl_u = math.floor(u / 2)
-        for m in range(k + 1):
-            self._cache[l, fl_u] = self.pr_exactly_k(l, fl_u, m)
-            self._cache[fl_u + 1, u] = self.pr_exactly_k(fl_u + 1, u, k - m)
-            pm.append(self._cache[l, fl_u] * self._cache[fl_u + 1, u])
-        self.pm[(l, u)] = pm
-
-        return sum(pm)
+        return a[..., 1:]
 
     def log_prob(self, value: torch.Tensor) -> torch.Tensor:
         unnormalized_log_prob = self._bernoulli.log_prob(value).sum(-1)
@@ -67,22 +68,20 @@ class KSubsetDistribution(torch.distributions.ExponentialFamily):
             for i in range(self.n, 0, -1):
                 # TODO: This indexing with j probably doesn't work.
                 p = self.a[..., i-1, :][iterator, j-1]
-                p[j-1 < 0] = 0.
-                bern_prob = p * self.probs[..., i - 1] / self.a[..., i, :][iterator, j]
+                p_nomin = self.a[..., i, :][iterator, j]
+                if self.log_space:
+                    p[j-1 < 0] = -float('inf')
+                    log_bern_prob = p + self.probs[..., i - 1].log() - p_nomin
+                    bern_prob = log_bern_prob.exp()
+                else:
+                    p[j-1 < 0] = 0.
+                    bern_prob = p * self.probs[..., i - 1] / p_nomin
                 zi = torch.bernoulli(bern_prob)
                 j = torch.where(zi == 1, j - 1, j)
                 samples.append(zi)
             samples = torch.stack(samples, dim=-1)
             return samples.squeeze()
             # return self._sample(shape, 0, self.probs.shape[-1], self.K)
-
-    def _sample(self, sample_shape, l: int, u: int, k: int):
-        # Algorithm 6 from paper. Requires algorithm 5 which doesn't work
-        probs = self.pm[(l, u)].reshape(-1, k)
-        m_star = torch.multinomial(probs, sample_shape.numel(), True)
-        z_lower = self._sample((1,), l, math.floor(u / 2), m_star)
-        z_upper = self._sample((1,), math.floor(u / 2) + 1, u, k - m_star)
-        return torch.cat([z_lower, z_upper], dim=-1)
 
 
 if __name__ == "__main__":
