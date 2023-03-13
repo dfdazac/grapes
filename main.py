0527554bed9f6b8e3543b5eb465e1422d1cc9e6c
Daniel <dfdazac@gmail.com>
2023-03-13 15:17:38 +0100

    WIP: building adjacency for selecting neighbors

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 class Arguments(Tap):
     dataset: str = 'cora'
+    num_hops: int = 2
     notes: str = None
     log_wandb: bool = False
     batch_size: int = 16
@@ -38,32 +39,48 @@ def train(args: Arguments):
 
     train_idx = data.train_mask.nonzero()
     train_num_batches = max(math.ceil(len(train_idx)/args.batch_size), 1)
-    train_batch_size = min(args.batch_size, len(data.train_mask))
-
+    batch_size = min(args.batch_size, len(data.train_mask))
+    adjacency = torch.sparse_coo_tensor(indices=data.edge_index,
+                                        values=torch.ones(data.edge_index.shape[-1]),
+                                        size=(data.num_nodes, data.num_nodes))
 
     max_epochs = 100
-    with tqdm(range(max_epochs)) as bar:
-        for epoch in bar:
-            if args.batch_size > 0:
-                for batch_id in range(0, train_num_batches):
-                    if batch_id == train_num_batches-1:
-                        batch_node_idx = train_idx[batch_id * train_batch_size:]
-                    else:
-                        batch_node_idx = train_idx[batch_id*train_batch_size:(batch_id+1)*train_batch_size]
+    with tqdm(range(max_epochs)) as loop:
+        for epoch in loop:
+            for batch_id in range(train_num_batches):
+                if batch_id == train_num_batches-1:
+                    batch_nodes = train_idx[batch_id * batch_size:]
+                else:
+                    batch_nodes = train_idx[batch_id*batch_size:(batch_id+1)*batch_size]
 
-            logits = model(data.x, data.edge_index)
-            loss = loss_fn(logits[data.train_mask], data.y[data.train_mask])
+                # Here's where we use GCN-GC to sample
+                for hop in args.num_hops:
+                    # Get neighborhoods of nodes in batch
+                    neighborhoods = get_neighborhoods(batch_nodes, adjacency)
 
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+                    # Pass neighborhoods to GCN-GF
 
-            accuracy = evaluate(logits, y, data.val_mask)
-            wandb.log({'valid-accuracy': accuracy})
-            wandb.log({'loss': loss.item()})
+                    # Get probabilities for sampling each node
 
-            bar.set_postfix({'loss': loss.item(), 'valid_acc': accuracy},
-                            refresh=False)
+                    # Sample Ai using the probabilities
+
+                    # Update batch_nodes
+
+                # Pass A1, A2, ... to GCN-C
+
+                logits = model(data.x, data.edge_index)
+                loss = loss_fn(logits[data.train_mask], data.y[data.train_mask])
+
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+                accuracy = evaluate(logits, y, data.val_mask)
+                wandb.log({'valid-accuracy': accuracy})
+                wandb.log({'loss': loss.item()})
+
+                loop.set_postfix({'loss': loss.item(), 'valid_acc': accuracy},
+                                refresh=False)
 
     test_accuracy = evaluate(logits, y, data.test_mask)
     print(f'Test accuracy: {test_accuracy:.1%}')
