58fabba7042d4bafd927ab6aa8ac9ba0f8eb6cdf
Taraneh Younesian <t.yoonesian@gmail.com>
2023-03-13 16:02:14 +0100

    getting the neighbors from sparse adj

--- modules/utils.py
+++ modules/utils.py
@@ -50,12 +50,15 @@ def get_neighboring_nodes(nodes, adjecency_matrix):
     assert type(nodes) == torch.Tensor  # nodes should be a tensor
     assert type(adjecency_matrix) == torch.Tensor  # adjecency_matrix should be a tensor
 
+    isin = torch.isin(adjecency_matrix._indices()[0], nodes)
+    edge_index = adjecency_matrix._indices()[torch.where(isin)[0]]
+
+
     # Get the neighboring nodes for each node in `nodes`
-    x = [adjecency_matrix[node, :].nonzero().squeeze() for node in nodes]
+    # x = [adjecency_matrix[node, :].nonzero().squeeze() for node in nodes]
 
     # Convert the list of neighboring nodes to a tensor
-    x = torch.stack(x)
-    return x
+    return edge_index
 
 
 x = get_neighboring_nodes(torch.tensor([0, 1, 2]), torch.tensor([[0, 1, 1], [1, 0, 1], [1, 1, 0]]))
