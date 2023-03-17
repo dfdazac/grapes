fix: Stability of log-prob in simple

main.py
@@ -38,9 +38,9 @@ def train(args: Arguments):
 
     gcn_c = GCN(data.num_features, hidden_dims=[32, num_classes]).to(device)
     gcn_gf = GCN(data.num_features, hidden_dims=[32, 1]).to(device)
-    log_z = torch.tensor(10., requires_grad=True).to(device)
+    log_z = torch.tensor(80., requires_grad=True).to(device)
     optimizer_c = Adam(gcn_c.parameters(), lr=1e-2)
-    optimizer_gf = Adam(list(gcn_gf.parameters()) + [log_z], lr=1e-2)
+    optimizer_gf = Adam(list(gcn_gf.parameters()) + [log_z], lr=1e-4)
     loss_fn = nn.CrossEntropyLoss()
 
     train_idx = data.train_mask.nonzero()
@@ -142,7 +142,7 @@ def train(args: Arguments):
                 loss_gfn.backward()
                 optimizer_gf.step()
 
-                print("Classification loss", loss_c, "GFN loss", loss_gfn)
+                # print("Classification loss", loss_c, "GFN loss", loss_gfn)
                 accuracy = evaluate(gcn_c, data, y, data.val_mask)
                 wandb.log({'valid-accuracy': accuracy})
                 wandb.log({'loss': loss_c.item()})

modules/simple.py
@@ -60,7 +60,7 @@ class KSubsetDistribution(torch.distributions.ExponentialFamily):
 
     def log_prob(self, value: torch.Tensor) -> torch.Tensor:
         unnormalized_log_prob = self._bernoulli.log_prob(value).sum(-1)
-        return unnormalized_log_prob - self.partition.log()
+        return unnormalized_log_prob - self.log_partition
 
     def sample(self, sample_shape=torch.Size()):
         # TODO: This won't work for general-shaped probs...