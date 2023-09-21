78d0b80c7ea3ea3cfec52f56a7ccd117cd8eb337
Taraneh Younesian <t.yoonesian@gmail.com>
2023-09-21 17:31:04 +0200

    calculated entropy in bits

--- modules/utils.py
+++ modules/utils.py
@@ -47,7 +47,10 @@ def sample_neighborhoods_from_probs(logits: torch.Tensor,
 
     samples = torch.topk(perturbed_log_probs, k=k, dim=0, sorted=False)[1]
 
-    entropy = b.entropy()
+    # entropy = b.entropy()
+    # calculate the entropy in bits
+    entropy = -(b.probs * b.probs.log2() + (1 - b.probs) * (1 - b.probs).log2())
+
     min_prob = b.probs.min(-1)[0]
     max_prob = b.probs.max(-1)[0]
 
