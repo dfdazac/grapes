0ed0b288d27723c9d1d13da62388d6ad22c7805b
Daniel <dfdazac@gmail.com>
2023-09-28 18:08:54 +0200

    Update values with full split on GAS

--- significance.py
+++ significance.py
@@ -4,13 +4,13 @@ import scikit_posthocs as sp
 from scipy.stats import wilcoxon
 
 data = np.array([
-    # Samples (datasets) ->                                    ↓ Treatments
+    # Samples (datasets) ->                                   ↓ Treatments
     [74.93, 53.26, 53.73, 38.02, 41.06, 26.42, 29.91, 33.01],  # FastGCN
     [69.90, 67.85, 56.36, 81.88, 43.23, 16.02, 44.58, 67.72],  # LADIES
     [86.20, 77.63, 83.07, 80.50, 48.47, 33.13, 53.71, 59.57],  # GraphSAINT
-    [80.70, 70.42, 79.22, 94.83, 51.32, 33.79, 69.38, 75.12],  # GAS
+    [87.67, 79.37, 87.94, 94.83, 51.32, 33.79, 69.38, 75.12],  # GAS
     [86.42, 78.80, 89.76, 92.44, 48.21, 30.99, 65.95, -1.00],  # AS-GCN
-    [87.29, 75.17, 90.11, 93.68, 47.33, 44.91, 64.54, 73.65]   # GFGS
+    [87.29, 78.74, 90.11, 93.68, 47.33, 44.91, 64.54, 73.65]   # GFGS
 ])
 
 no_asgcn = np.concatenate((data[:4], data[5:6]))
@@ -18,9 +18,10 @@ print(f'Means without AS-GCN={np.mean(no_asgcn, axis=1)}')
 print(f'AS-GCN mean={np.mean(data[4, :7])}')
 
 print(ss.friedmanchisquare(*data))
-print(sp.posthoc_nemenyi_friedman(data.T) <= 0.05)
+print(sp.posthoc_nemenyi_friedman(data.T))
 
 ranks = np.argsort(np.argsort(-data, axis=0), axis=0) + 1
+print(ranks)
 print(np.sum(ranks, axis=1))
 print(f'mean_ranks={np.mean(ranks, axis=1)}\n')
 
@@ -31,5 +32,5 @@ num_comparisons = len(models) - 1
 for i in range(len(models) - 1):
     model_ranks = ranks[i]
     stat, p = wilcoxon(gfgs_ranks, model_ranks)
-    p = p * num_comparisons
-    print(f'{models[i]}: stat={stat}, p={p}' + ('***' if p < 0.05 else ''))
+    # p = p * num_comparisons
+    print(f'{models[i]}: stat={stat}, p={p}' + ('***' if p < 0.05/5.0 else ''))
