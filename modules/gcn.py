0a30cd4094f16afe3a8c4971c1b762faacea4bee
Taraneh Younesian <t.yoonesian@gmail.com>
2024-02-12 11:36:52 +0100

    myGCN bug fixed

--- modules/gcn.py
+++ modules/gcn.py
@@ -183,13 +183,14 @@ class PNA(nn.Module):
         return x
 
 
-
 class MyGCNConv(nn.Module):
     def __init__(self,
                  in_channels: int,
                  out_channels: int):
         super(MyGCNConv, self).__init__()
 
+        self.in_channels = in_channels
+        self.out_channels = out_channels
         self.w = nn.Parameter(torch.FloatTensor(in_channels, out_channels))
         # nn.init.xavier_uniform_(self.w)
 
